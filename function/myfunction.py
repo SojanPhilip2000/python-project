e8bc24ae414ec2a1f4fa931f21c2b485c6e56592
Sojan Philip <83229006+SojanPhilip2000@users.noreply.github.com>
2023-05-17 23:31:04 +0530

    Add files via upload

---
+++ function/myfunction.py
@@ -0,0 +1,11 @@
+#name age mobile company salery
+
+def employeeid(name,age,mob_num, comp_name,salery):
+    print("Name:"+name)
+    print("age:",age)
+    print("mobile number:", mob_num)
+    print("company name:",comp_name)
+    print("salery:",salery)
+employeeid("sojan",7,9562541016,"infosys",30000)
+employeeid("joppan",24,950865776,"infosys",9000)
+employeeid(comp_name="mphasis",age=23,mob_num=9565477516,name="meljo",salery=30000)
